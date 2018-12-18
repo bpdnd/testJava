2fb203dad155281db522f5c2cd7fd01f5e03b49e
bpdnd <3438291751@qq.comgit config --global user.email 3438291751@qq.com>
2018-12-18 14:36:06 +0800

    添加class类

--- src/testJava/test.java
+++ src/testJava/test.java
@@ -1,10 +1,14 @@
 package testJava;
+import testJava.Person;
 
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println(1234);
+        Person person = new Person();
+        person.name = "姓名";
+        person.age  = "12";
+        person.sex  = "男";
+        System.out.println(person.name+person.age+person.sex);
 	}
-
 }
